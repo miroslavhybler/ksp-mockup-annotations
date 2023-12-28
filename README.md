efad9bebeed56e4d09f9db2331c29ec2a0fdf469
Miroslav Hýbler <miroslav@peko-studio.cz>
2023-12-28 10:39:19 +0100

    Initial commit, v1.1.2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ksp-mockup-annotations
+
+Just annotations for [ksp-mockup](https://github.com/miroslavhybler/ksp-mockup).
---
+++ mockup-annotations/build.gradle.kts
@@ -0,0 +1,43 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+    id("maven-publish")
+}
+
+group ="com.github.miroslavhybler.mockup-annotations"
+version= "1.1.2"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+kotlin {
+    jvmToolchain(jdkVersion = 8)
+}
+
+dependencies {
+    implementation("androidx.annotation:annotation:1.7.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from (components.getByName("kotlin"))
+                groupId = "mir.oslav.mockup"
+                artifactId = "annotations"
+                version = "1.1.2"
+                pom {
+                    description.set("Jitpack.io deploy")
+                }
+            }
+
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
+}
