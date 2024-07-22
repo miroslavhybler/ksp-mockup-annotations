6d06f3551a55535210f8ec38c83ee5427230e459
Miroslav Hýbler <miroslav@peko-studio.cz>
2024-07-22 13:50:27 +0200

    v1.1.7 - compatibility commit, added support of IgnoreOnMockupAnnotation for processor

--- mockup-annotations/build.gradle.kts
+++ mockup-annotations/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.miroslavhybler.mockup-annotations"
-version = "1.1.6"
+version = "1.1.7"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
--- mockup-annotations/src/main/kotlin/com/mockup/annotations/IgnoreOnMockup.kt
+++ mockup-annotations/src/main/kotlin/com/mockup/annotations/IgnoreOnMockup.kt
@@ -9,10 +9,10 @@ package com.mockup.annotations
 
 
 /**
- * Annotates class's property to be skipped during generating mockup data
+ * Annotates class's property to be skipped during generating mockup data.
  * @author Miroslav Hýbler<br>
  * created on 01.06.2024
- * @since 1.1.6
+ * @since 1.1.7
  */
 @Target(AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.SOURCE)
