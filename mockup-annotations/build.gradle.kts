7883b956b5f75b3ef63c2f302c794a2340ac916b
Miroslav Hýbler <miroslav@peko-studio.cz>
2026-08-16 12:10:33 +0200

    🚀 v2.0.0-beta03 - Compatibility release

--- mockup-annotations/build.gradle.kts
+++ mockup-annotations/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.miroslavhybler.mockup-annotations"
-version = "2.0.0-beta02"
+version = "2.0.0-beta03"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -32,7 +32,7 @@ afterEvaluate {
                 from(components.getByName("kotlin"))
                 groupId = "com.github.miroslavhybler"
                 artifactId = "mockup-annotations"
-                version = "2.0.0-beta02"
+                version = "2.0.0-beta03"
                 pom {
                     description.set("Jitpack.io deploy")
                 }
